19e6f5b6175367d798ee28564d565951ba8fff1d
Doug Harple <dharple@gmail.com>
2021-01-29 22:43:37 -0500

    Confirmed Behat handles Braille (resolves #1)

--- tests/Filter/AsciiTest.php
+++ tests/Filter/AsciiTest.php
@@ -69,6 +69,10 @@ class AsciiTest extends FilterTest
             'output' => 'Hello world, Kalemera kosme',
         ],
 
+        [
+            'input' => '⠓⠑⠇⠇⠕ ⠺⠕⠗⠇⠙',
+            'output' => 'hello world', 
+        ],
     ];
 
     /**
